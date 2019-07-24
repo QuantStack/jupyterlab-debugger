de13301585037a4f0d97c1847d94dc3162d19616
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2019-07-24 13:34:27 +0200

    Get current notebook widget

--- src/widget.ts
+++ src/widget.ts
@@ -9,25 +9,39 @@ export class Debugger extends Widget {
     super();
     this.addClass(DEBUGGER_WIDGET_CLASS);
 
+    this._tracker = options.tracker;
+
     this.toolbar = new Toolbar<Widget>();
     let start = new ToolbarButton({
-      iconClassName: 'jp-BugIcon',
-      onClick: () => {
-          console.log('Start Debugger');
+      iconClassName: "jp-BugIcon",
+      onClick: async () => {
+        const widget = this._tracker.currentWidget;
+        if (!widget) {
+          return;
+        }
+        // const kernel = widget.context.session.kernel;
+        // kernel.sendControlMessage();
+        console.log("Start Debugger");
       },
-      tooltip: 'Start Debugger'
+      tooltip: "Start Debugger"
     });
 
     let stop = new ToolbarButton({
-      iconClassName: 'jp-StopIcon',
+      iconClassName: "jp-StopIcon",
       onClick: () => {
-          console.log('Stop Debugger');
+        const widget = this._tracker.currentWidget;
+        if (!widget) {
+          return;
+        }
+        // const kernel = widget.context.session.kernel;
+        // kernel.sendControlMessage();
+        console.log("Stop Debugger");
       },
-      tooltip: 'Stop Debugger'
+      tooltip: "Stop Debugger"
     });
 
-    this.toolbar.addItem('start-debugger', start);
-    this.toolbar.addItem('stop-debugger', stop);
+    this.toolbar.addItem("start-debugger", start);
+    this.toolbar.addItem("stop-debugger", stop);
 
     let layout = new PanelLayout();
     layout.addWidget(this.toolbar);
@@ -36,10 +50,11 @@ export class Debugger extends Widget {
   }
 
   readonly toolbar: Toolbar<Widget>;
+  private _tracker: INotebookTracker;
 }
 
 export namespace Debugger {
-    export interface IOptions {
-        tracker: INotebookTracker;
-    }
+  export interface IOptions {
+    tracker: INotebookTracker;
+  }
 }
