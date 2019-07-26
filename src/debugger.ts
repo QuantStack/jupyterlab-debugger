a7917b2782c0fdcbe4f702f13480461c0633193d
Wolf Vollprecht <w.vollprecht@gmail.com>
2019-07-26 19:25:29 +0200

    add debug session and messages

--- src/debugger.ts
+++ src/debugger.ts
@@ -1,7 +1,9 @@
 import { Cell } from "@jupyterlab/cells";
 import { CodeMirrorEditor } from "@jupyterlab/codemirror";
-import { INotebookTracker } from "@jupyterlab/notebook";
+import { INotebookTracker, NotebookPanel } from "@jupyterlab/notebook";
 import { Signal } from "@phosphor/signaling";
+
+import { KernelMessage } from "@jupyterlab/services";
 
 export interface IBreakpointEvent {
   line: number;
@@ -12,6 +14,144 @@ export interface IBreakpointEvent {
 export interface IBreakpoint {
   text: string;
   line: number;
+}
+
+export class DebugSession {
+  notebook: NotebookPanel;
+  seq: number;
+
+  constructor(notebook: NotebookPanel)
+  {
+    this.notebook = notebook;
+  }
+
+  create_request_msg(command: string, args: any)
+    : KernelMessage.IDebugRequestMsg['content']
+  {
+    this.seq += 2;
+    return {
+      seq: this.seq - 2,
+      type: 'request',
+      command: command,
+      arguments: args
+    } as KernelMessage.IDebugRequestMsg['content']
+  }
+
+  create_attach_msg(cwd: string, justMyCode: boolean)
+  {
+    return this.create_request_msg('attach', {
+      cwd: cwd,
+      justMyCode: justMyCode
+    });
+  }
+
+  create_evaluate_msg(code: string) 
+  {
+    return this.create_request_msg('evaluate', {
+      expression: code
+    });
+  }
+
+  create_stacktrace_request(threadId: number) 
+  {
+    return this.create_request_msg('stacktrace', {threadId: threadId});
+  }
+
+  create_scopes_request(frameId: number) 
+  {
+    return this.create_request_msg('scopes', {frameId: frameId});
+  }
+
+  create_variables_request(variablesReference: number) 
+  {
+    return this.create_request_msg('variables', {variablesReference: variablesReference});
+  }
+
+  create_continue_request(threadId: number) 
+  {
+    return this.create_request_msg('continue', {threadId: threadId});
+  }
+
+  create_update_cell_request(cellId: number, nextId: number, code: string) 
+  {
+    return this.create_request_msg('update_cell', {
+      cellId: cellId,
+      nextId: nextId,
+      code: code
+    });
+  }
+
+  create_breakpoint_request(path: string, lineNumbers: number[])
+  {
+    let breakarray = []
+    for (let el in lineNumbers)
+    {
+      breakarray.push({line: el});
+    }
+
+    return this.create_request_msg('setBreakpoints',
+    {
+      source: { path: path }, 
+      breakpoints: breakarray,
+      lines: lineNumbers,
+      sourceModified: false
+    });
+  }
+
+  create_configuration_done_msg()
+  {
+    return this.create_request_msg('configurationDone', {});
+  }
+
+  create_disconnect_msg(restart: boolean, terminateDebuggee: boolean)
+  {
+    return this.create_request_msg('disconnect', {
+      restart: restart,
+      terminateDebuggee: terminateDebuggee
+    });
+  }
+
+  create_next_msg(threadId: number)
+  {
+    return this.create_request_msg('next', {threadId: threadId});
+  }
+
+  create_initialize_msg()
+  {
+    return this.create_request_msg('initialize', {
+      clientId: 'vscode',
+      clientName: 'Visual Studio Code',
+      adapterID: 'python',
+      pathFormat: 'path',
+      linesStartAt1: true,
+      columnsStartAt1: true,
+      supportsVariableType: true,
+      supportsVariablePaging: true,
+      supportsRunInTerminalRequest: true,
+      locale: true
+    });
+  }
+
+  async start() {
+    this.seq = 0;
+    console.log("Start Debugger");
+    let kernel = this.notebook.session.kernel;
+    let debug_init = kernel.requestDebug(this.create_initialize_msg());
+    debug_init.onReply = (msg: KernelMessage.IDebugReplyMsg) => {
+      console.log("received init reply");
+      console.log(msg)
+    };
+    await debug_init.done;
+
+    console.log("did i already receive the reply? moving to attach...");
+
+    let debug_attach = kernel.requestDebug(this.create_attach_msg('/tmp/', false));
+    debug_attach.onReply = (msg: KernelMessage.IDebugReplyMsg) => {
+      console.log("received attach reply");
+      console.log(msg)
+    };
+    await debug_attach.done;
+  }
 }
 
 export class Debugger {
